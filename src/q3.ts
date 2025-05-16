ab652638f67ee08979fea4a2c43747e7b97d2039
Yoav Levin <y_levin@apple.com>
2025-05-16 22:09:18 +0300

    finish q3 passing all tests

--- src/q3.ts
+++ src/q3.ts
@@ -1,12 +1,11 @@
-import { AppExp, CExp, Exp, isAppExp, isBoolExp, isIfExp, isNumExp, isPrimOp, isProcExp, isStrExp, PrimOp, ProcExp, Program } from './L3/L3-ast';
+import { AppExp, CExp, DefineExp, Exp, isAppExp, isBoolExp, isDefineExp, isIfExp, isNumExp, isPrimOp, isProcExp, isProgram, isStrExp, isVarDecl, isVarRef, parseL3Exp, PrimOp, ProcExp, Program, VarDecl } from './L3/L3-ast';
+import fs from "fs";
 
 import { Result, makeFailure, mapResult, bind} from './shared/result';
 import { isNumber, isString } from './shared/type-predicates';
 import { makeOk } from './shared/result';
-import { Closure, isClosure, isCompoundSExp, isEmptySExp, isSymbolSExp } from './L3/L3-value';
-import exp from 'constants';
-import { mapv } from './shared/optional';
-import { verify } from 'crypto';
+import { ascend } from 'ramda';
+import { escape } from 'querystring';
 /*
 Purpose: Transform L2 AST to JavaScript program string
 Signature: l2ToJS(l2AST)
@@ -15,8 +14,41 @@ Type: [EXP | Program] => Result<string>
 
 type Value = number | boolean | string | PrimOp
 
+export const l2ToJS = (exp: Exp | Program): Result<string>  => 
+    isNumExp(exp) ? makeOk(valueToString(exp.val)) :
+    isBoolExp(exp) ? makeOk(valueToString(exp.val)) :
+    isStrExp(exp) ? makeOk(valueToString(exp.val)) :
+    isPrimOp(exp) ? makeOk(valueToString(exp)) :
+    isVarRef(exp) ? makeOk(exp.var) :
+    isIfExp(exp) ? bind(l2ToJS(exp.test), (test: string) =>
+                        bind(l2ToJS(exp.then), (then: string) =>
+                            bind(l2ToJS(exp.alt), (alt: string) =>
+                                makeOk(`(${test} ? ${then} : ${alt})`)
+                                )
+                            )
+                        ):
+    isProgram(exp) ? bind(mapResult((expression: Exp) => l2ToJS(expression), exp.exps), (exps: string[]) => makeOk(exps.join(';\n'))) : 
+    isAppExp(exp) ? convertAppExp(exp) :
+    isProcExp(exp) ? convertProcExp(exp) :
+    isDefineExp(exp) ? convertDefineExp(exp) :
+    makeFailure("bad expression")
+
+const operatorDict = {
+    '+': "+",
+    '-': "-",
+    '*': "*",
+    '/': "/",
+    '>': ">",
+    '<': "<",
+    '=': "===",
+    'eq?': "===",
+    'not': "!",
+    'and': "&&",
+    'or': "||",
+}
+
 const isBaseOp = (op: string) : boolean => 
-    ["+", "-", "*", "/", ">", "<"].includes(op);
+    ["+", "-", "*", "/", ">", "<", "=", "not", "and", "or", "eq?"].includes(op);
 
 
 const valueToString = (val: Value): string =>
@@ -27,44 +59,11 @@ const valueToString = (val: Value): string =>
     isPrimOp(val) ? primOpToString(val.op) :
     val;
 
-const eqPrim: string = `(x: any, y: any) => {
-    if (typeof(x) === 'number' && typeof(y) === 'number') {
-        return x === y;
-    }
-    else if (typeof(x) === 'string' && typeof(y) === 'string') {
-        return x === y;
-    }
-    else if (typeof(x) === 'boolean' && typeof(y) === 'boolean') {
-        return x === y;
-    }
-    else {
-        return false;
-    }
-};`
 
 const convertNonBaseOp = (op: string) : string =>
-    op === "=" ? "===" :
-    op === "and" ? "&&" :
-    op === "or" ? "||" :
-    op === "not" ? "!" :
-    op === "number?" ? "((x) => typeof(x) === 'number')(y)" :
-    op === "boolean?" ? "((x) => typeof(x) === 'boolean')(y)" :
-    op === "eq?" ? eqPrim : 
+    op === "number?" ? "((x) => typeof(x) === 'number')" :
+    op === "boolean?" ? "((x) => typeof(x) === 'boolean')" :
     op;
-
-
-
-
-export const l2ToJS = (exp: Exp | Program): Result<string>  => 
-    isNumExp(exp) ? makeOk(valueToString(exp.val)) :
-    isBoolExp(exp) ? makeOk(valueToString(exp.val)) :
-    isStrExp(exp) ? makeOk(valueToString(exp.val)) :
-    isPrimOp(exp) ? makeOk(valueToString(exp)) :
-    isIfExp(exp) ? makeOk(`${l2ToJS(exp.test)} ? (${l2ToJS(exp.then)}) : (${l2ToJS(exp.alt)})`) :
-    isAppExp(exp) ? convertAppExp(exp) :
-    isProcExp(exp) ? convertProcExp(exp) :
-    makeFailure("bad expression")
-
 
 
 const primOpToString = (op: string) : string => 
@@ -73,19 +72,33 @@ const primOpToString = (op: string) : string =>
 
 
 const convertProcExp = (exp: ProcExp) : Result<string> =>
-    bind(mapResult((param: Cexp) => ))
+    bind(mapResult((param: VarDecl) => makeOk(param.var), exp.args), (args: string[]) =>
+        bind(l2ToJS(exp.body[0]), (body: string) =>
+            (makeOk(`((${args.join(',')}) => ${body})`))
+        )
+    );
     
 const convertAppExp = (exp: AppExp): Result<string> =>
     isPrimOp(exp.rator) && isBaseOp(exp.rator.op)
         ? bind(
-            mapResult((rand: CExp) => l2ToJS(rand), exp.rands),
-            (rands: string[]) => makeOk(rands.join(` ${l2ToJS(exp.rator)} `))
+            mapResult((rand: CExp) => l2ToJS(rand), exp.rands), (rands: string[]) => 
+                bind(l2ToJS(exp.rator), (rator: string) =>
+                    rands.length > 1 ? makeOk(`(${rands.join(` ${operatorDict[rator as keyof typeof operatorDict]} `)})`) :
+                    makeOk(`(!${rands[0]})`)
+                )
+                
         )
-        : bind(
-            l2ToJS(exp.rator),
-            (op: string) =>
+        : bind(l2ToJS(exp.rator), (op: string) =>
                 bind(
-                    mapResult((rand: CExp) => l2ToJS(rand), exp.rands),
-                    (rands: string[]) => makeOk(`${op}(${rands.join("), (")})`)
+                    mapResult((rand: CExp) => l2ToJS(rand), exp.rands), (rands: string[]) => 
+                        rands.length >= 1 ? makeOk(`${op}(${rands.join(",")})`) :
+                        makeOk(`${op}()`)
                 )
         );
+
+
+const convertDefineExp = (exp: DefineExp) => 
+    bind(makeOk(exp.var.var), (variable: string) =>
+        bind(l2ToJS(exp.val), (body: string) =>
+            makeOk(`const ${variable} = ${body}`))
+    );
